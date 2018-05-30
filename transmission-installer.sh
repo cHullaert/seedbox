bb016380dfba6a9c92f79eb6ea3ed40cb5c5d324
Christof Hullaert <christof.hullaert@gmail.com>
2018-05-30 14:24:42 +0200

    update script

--- transmission-installer.sh
+++ transmission-installer.sh
@@ -10,15 +10,28 @@ while getopts "p:" opt; do
     esac
 done
 
+echo "password used: $password"
+
 shift $((OPTIND-1))
 
+echo "launch system update"
 apt update -y
 apt upgrade -y
+
+echo "install server"
 apt install transmission-daemon -y
-service transmission-daemon stop
-
-service transmission-daemon start
-
 apt install nginx -y
 
-service nginx restart
+echo "stop services"
+service transmission-daemon stop
+service nginx stop
+
+echo "download configurations"
+wget -q https://raw.githubusercontent.com/cHullaert/seedbox/master/settings.json -O /etc/transmission-daemon/settings.json
+wget -q https://raw.githubusercontent.com/cHullaert/seedbox/master/default -0 /etc/nginx/sites-available/default
+
+sed -i -e "s/\${PASSWORD}/$password/g" /etc/transmission-daemon/settings.json
+
+echo "start service"
+service transmission-daemon start
+service nginx start
